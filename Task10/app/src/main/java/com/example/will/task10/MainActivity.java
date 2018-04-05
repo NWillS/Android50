40c396ca6dd3877826840e9bcc2c98a8fb0a50e3
S N <>
2018-04-05 16:34:08 +0900

    Added. Task10

--- Task10/app/src/main/java/com/example/will/task10/MainActivity.java
+++ Task10/app/src/main/java/com/example/will/task10/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.will.task10;
 
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import java.security.SecureRandom;
+
+@SuppressWarnings("UnnecessarilyQualifiedInnerClassAccess")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.button);
+        final ImageView imageView = findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TypedArray typedArray = getApplicationContext().getResources().obtainTypedArray(R.array.default_img);
+                SecureRandom random = new SecureRandom();
+                Integer rand = random.nextInt(typedArray.length());
+                Drawable drawable = typedArray.getDrawable(rand);
+                imageView.setImageDrawable(drawable);
+                typedArray.recycle();
+            }
+        });
     }
 }
