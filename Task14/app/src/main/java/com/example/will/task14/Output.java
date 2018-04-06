19ccdceba2e0f450fd692eae8b66d13461700144
S N <>
2018-04-06 17:40:29 +0900

    Added. Task14

---
+++ Task14/app/src/main/java/com/example/will/task14/Output.java
@@ -0,0 +1,35 @@
+package com.example.will.task14;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class Output extends Fragment {
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_output, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.textView = (TextView) view.findViewById(R.id.textView);
+    }
+
+    void setTextView(String text){
+        this.textView.setText(text);
+    }
+}
