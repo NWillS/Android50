dd1f8debccdac9a875af5d208821232c266ef14a
S N <>
2018-04-10 13:43:09 +0900

    Updated. Fragmentのファイルを一つにする

--- Task20/app/src/main/java/com/example/will/task20/TestViewPager.java
+++ Task20/app/src/main/java/com/example/will/task20/TestViewPager.java
@@ -4,31 +4,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-class TestViewPager extends FragmentPagerAdapter {
-    private static final int PAGE_NUM = 3;
+import java.util.List;
 
-    TestViewPager(FragmentManager fm) {
+class TestViewPager extends FragmentPagerAdapter {
+    List<Page> pageList;
+
+    TestViewPager(FragmentManager fm, List<Page> pageList) {
         super(fm);
+        this.pageList = pageList;
     }
 
     @Override
     public Fragment getItem(int position) {
         Fragment fragment;
-        switch (position){
-            case 0:
-                fragment = new FirstPageFragment();
-                break;
-            case 1:
-                fragment = new SecondPageFragment();
-                break;
-            default:
-                fragment = new ThirdPageFragment();
-        }
+        fragment = PageFragment.newInstance(pageList,position);
         return fragment;
     }
 
     @Override
     public int getCount() {
-        return PAGE_NUM;
+        return pageList.size();
     }
 }
