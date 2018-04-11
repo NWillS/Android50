61a6696302175462c3d90f53e5d9be17a7a9198f
S N <>
2018-04-11 16:09:34 +0900

    Updated. Insert

--- Task25/app/src/main/java/com/example/will/task25/DatabaseHelper.java
+++ Task25/app/src/main/java/com/example/will/task25/DatabaseHelper.java
@@ -8,14 +8,6 @@ class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION           = 1;
     private static final String DATABASE_NAME           = "test_database";
 
-    private static final String TABLE_TODO             = "tr_todo";
-    private static final String COLUMN_TODO_ID         = "todo_id";
-    private static final String COLUMN_TODO_TITLE      = "todo_title";
-    private static final String COLUMN_TODO_CONTENTS   = "todo_contents";
-    private static final String COLUMN_CREATED         = "created";
-    private static final String COLUMN_MODIFIED        = "modified";
-    private static final String COLUMN_LIMIT_DATE      = "limit_date";
-    private static final String COLUMN_DELETE_FLG      = "delete_flg";
 
     DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,19 +15,20 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String CREATE_TODO_TABLE = "create table " + TABLE_TODO + " ( " + COLUMN_TODO_ID + " integer primary key, " +
-                COLUMN_TODO_TITLE +  " text, " +
-                COLUMN_TODO_CONTENTS + " text, " +
-                COLUMN_CREATED + " text, " +
-                COLUMN_MODIFIED + " text, " +
-                COLUMN_LIMIT_DATE + " text, " +
-                COLUMN_DELETE_FLG +  " integer" +" ) ";
+        String CREATE_TODO_TABLE = "create table " + FeedReaderContract.FeedEntry.TABLE_TODO + " ( " +
+                FeedReaderContract.FeedEntry.COLUMN_TODO_ID + " integer primary key autoincrement, " +
+                FeedReaderContract.FeedEntry.COLUMN_TODO_TITLE +  " text, " +
+                FeedReaderContract.FeedEntry.COLUMN_TODO_CONTENTS + " text, " +
+                FeedReaderContract.FeedEntry.COLUMN_CREATED + " text, " +
+                FeedReaderContract.FeedEntry.COLUMN_MODIFIED + " text, " +
+                FeedReaderContract.FeedEntry.COLUMN_LIMIT_DATE + " text, " +
+                FeedReaderContract.FeedEntry.COLUMN_DELETE_FLG +  " integer" + " ) ";
 
         sqLiteDatabase.execSQL(CREATE_TODO_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO);
+
     }
 }
