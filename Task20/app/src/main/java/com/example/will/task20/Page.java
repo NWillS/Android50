4ac2536c872864cb5cd3b32c1d6dc67ea5844fe7
S N <>
2018-04-12 11:05:17 +0900

    Update. 必要ファイルが追加出来ていなかった

---
+++ Task20/app/src/main/java/com/example/will/task20/Page.java
@@ -0,0 +1,20 @@
+package com.example.will.task20;
+
+public class Page {
+    private String text;
+
+    public Page(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+
+        return this.text;
+    }
+
+    public void setText(String text) {
+
+        this.text = text;
+    }
+
+}
