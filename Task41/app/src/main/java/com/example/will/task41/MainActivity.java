530059527b8b08ee107e81497145fd0c58af2e26
S N <>
2018-04-19 15:34:51 +0900

    Added. Lint

--- Task41/app/src/main/java/com/example/will/task41/MainActivity.java
+++ Task41/app/src/main/java/com/example/will/task41/MainActivity.java
@@ -1,21 +1,17 @@
 package com.example.will.task41;
 
-import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.os.SharedMemory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
-    SeekBar alphaSeekBar;
-    SeekBar redSeekBar;
-    SeekBar greenSeekBar;
-    SeekBar blueSeekBar;
-    ImageView imageView;
+    private SeekBar alphaSeekBar;
+    private SeekBar redSeekBar;
+    private SeekBar greenSeekBar;
+    private SeekBar blueSeekBar;
+    private ImageView imageView;
 
 
     @Override
@@ -39,10 +35,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     }
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-        int newColor = alphaSeekBar.getProgress() * 0x1000000
-                + redSeekBar.getProgress() * 0x10000
-                + greenSeekBar.getProgress() * 0x100
+    public void onProgressChanged(SeekBar seekBar, int i, boolean bo) {
+        int newColor = (alphaSeekBar.getProgress() << 24)
+                + (redSeekBar.getProgress() << 16)
+                + (greenSeekBar.getProgress() << 8)
                 + blueSeekBar.getProgress();
         imageView.setColorFilter(newColor, PorterDuff.Mode.MULTIPLY);
     }
