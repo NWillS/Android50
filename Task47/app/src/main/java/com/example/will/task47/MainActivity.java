ffe7b1f111406bcc8472c6a1c8b0a716f928f3b7
S N <seidi.nakamura@st-ventures.jp>
2018-04-25 15:05:55 +0900

    Fixed. 不要なコード削除（FirebaseAnalytics）

--- Task47/app/src/main/java/com/example/will/task47/MainActivity.java
+++ Task47/app/src/main/java/com/example/will/task47/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.will.task47;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
 
-@SuppressWarnings("UnnecessarilyQualifiedInnerClassAccess")
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
