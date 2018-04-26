b984f16b1cb29c46339125f715eec60c34caae58
S N <seidi.nakamura@st-ventures.jp>
2018-04-26 19:21:53 +0900

    Fixed. ボタン押下でGPS取得を一回取得に変更

--- Task45/app/src/main/java/com/example/will/task45/MainActivity.java
+++ Task45/app/src/main/java/com/example/will/task45/MainActivity.java
@@ -8,11 +8,14 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -24,10 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private boolean isUpdating;
     private Button button;
 
     private GPSChecker gpsChecker;
+    private final int REQUEST_PERMISSION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,44 +38,30 @@ public class MainActivity extends AppCompatActivity {
         Log.e(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
-        gpsChecker = new GPSChecker(this);
+        checkPermission();
 
         button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isUpdating) {
-                    button.setText("START");
-                    isUpdating = false;
-                    stopGPS();
-                } else {
-                    button.setText("STOP");
-                    isUpdating = true;
-                    startGPS();
-                }
+                checkPermission();
+                getGPS();
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        button.setText("START");
-        isUpdating = false;
-    }
+
 
     @Override
     protected void onPause() {
-        stopGPS();
+        if(gpsChecker != null) {
+            gpsChecker.stopUsingGPS();
+        }
         super.onPause();
     }
 
-    private void stopGPS(){
-        gpsChecker.stopUsingGPS();
-    }
-
-    private void startGPS(){
+    private void getGPS(){
         if(gpsChecker.canGetLocation){
             double latitude = gpsChecker.getLatitude();
             double longitude = gpsChecker.getLongitude();
@@ -81,4 +70,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void checkPermission() {
+        // 既に許可している
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+
+            gpsChecker = new GPSChecker(this);
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this,
+                    "許可されないとアプリが実行できません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},
+                    REQUEST_PERMISSION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION){
+            gpsChecker = new GPSChecker(this);
+        }
+    }
 }
