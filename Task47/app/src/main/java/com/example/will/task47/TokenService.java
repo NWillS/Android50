f31e5df153d67a8ff4186f77e34d1eac0e0a6ba7
S N <seidi.nakamura@st-ventures.jp>
2018-04-29 20:03:29 +0900

    Added. FCM registration token

---
+++ Task47/app/src/main/java/com/example/will/task47/TokenService.java
@@ -0,0 +1,20 @@
+package com.example.will.task47;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class TokenService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.w("notification", refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+    }
+}
